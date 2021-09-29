142f28ec65f0ac0a2d188e00d983bc93969e8eca
VictorSardellaro <victor.henryk@outlook.com>
2021-09-29 11:06:56 -0300

    Initial

---
+++ Classes/Serie.cs
@@ -0,0 +1,7 @@
+namespace DIO_PJ_CRUD.Classes
+{
+    public abstract class EntidadeBase{
+        public int Id { get; protected set; }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DIO_PJ_CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntidadeBase minhaClasse = new EntidadeBase();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
