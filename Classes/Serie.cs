df020a5f36c599505b52d84382baff1ca6a70918
VictorSardellaro <victor.henryk@outlook.com>
2021-09-30 13:47:17 -0300

    metodo excluir

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -12,6 +12,7 @@ namespace DIO_Series.Classes
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         //Atributos
@@ -19,6 +20,7 @@ namespace DIO_Series.Classes
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+        private bool Excluido { get; set; }
 
 
         public override string ToString()
@@ -41,6 +43,11 @@ namespace DIO_Series.Classes
         public int retornaId()
         {
             return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
         }
 
     }
