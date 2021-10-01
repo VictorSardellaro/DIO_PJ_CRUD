c0056bd786b449bfd4d061f76e7c1a4d134e9bf5
VictorSardellaro <victor.henryk@outlook.com>
2021-10-01 10:17:58 -0300

    Espaço apos ano de inicio

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -29,7 +29,7 @@ namespace DIO_Series.Classes
             retorno += $"Gênero: {this.Genero} {Environment.NewLine}";
             retorno += $"Titulo: {this.Titulo} {Environment.NewLine}";
             retorno += $"Descrição: {this.Descricao} {Environment.NewLine}";
-            retorno += $"Ano de inicio: {this.Ano}";
+            retorno += $"Ano de inicio: {this.Ano} {Environment.NewLine}";
             retorno += $"Excluido: {this.Excluido}";
 
             return retorno;
@@ -43,6 +43,11 @@ namespace DIO_Series.Classes
         public int retornaId()
         {
             return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
         }
 
         public void Excluir()
